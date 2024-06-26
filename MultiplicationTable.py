aeac5bb26455a7e020b7060cc96d657c872e5cb2
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2024-06-26 19:10:02 +0530

    Delete MultiplicationTable.py

--- MultiplicationTable.py
+++
@@ -1,8 +0,0 @@
-def multiply(number):
-    i = 1
-    while i <= 10:
-        print(number*i)
-        i += 1
-    
-number = int(input("enter the number you want the multiplication table of: "))
-multiply(number)
